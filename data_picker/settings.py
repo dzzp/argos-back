e961be11c8a8ca5be3ab246d44ff0e14c1d5d9db
punkyoon <cryptography@konkuk.ac.kr>
2017-09-09 16:31:53 +0000

    change db

--- data_picker/settings.py
+++ data_picker/settings.py
@@ -73,11 +73,23 @@ WSGI_APPLICATION = 'data_picker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+DATABASES = {
+    'defaults': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'datapicker',
+        'USER': 'pickeruser',
+        'PASSWORD': 'temporaryP@SSW0rD',
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': '',
     }
 }
 
