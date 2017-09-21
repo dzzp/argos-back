6bcab4235f5d91d72038488d290fd2793abf6287
punkyoon <cryptography@konkuk.ac.kr>
2017-09-21 12:41:31 +0000

    added a file validator

--- video/validators.py
+++ video/validators.py
@@ -1,3 +1,4 @@
+import os
 import re
 from django.forms import ValidationError
 
@@ -5,3 +6,20 @@ from django.forms import ValidationError
 def lnglat_validator(value):
     if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
         raise ValidationError('Invalid Lng, Lat Type')
+
+
+def file_validator(value):
+    file_types = ('.mov', '.mp4', '.avi')
+    file_ext = os.path.splitext(value)
+
+    if file_ext[-1] in file_types:
+        if is_video_exists(value):
+            return True
+    
+    return False
+
+def is_video_exists(value):
+    if os.path.isfile(value):
+        return True
+    else:
+        return False
