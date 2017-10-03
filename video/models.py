49b4018d5475bf6800c3b88c912afa50c0b3a88a
punkyoon <cryptography@konkuk.ac.kr>
2017-10-03 03:05:49 +0000

    added Person model

--- video/models.py
+++ video/models.py
@@ -7,7 +7,7 @@ from video.validators import lnglat_validator
 class Video(models.Model):
     _id = models.AutoField(primary_key=True)
     video_path = models.TextField(unique=True)
-    time = models.DateTimeField(auto_now=True)
+    time = models.DateTimeField()
     location = models.CharField(
         max_length=50,
         blank=True,
@@ -15,10 +15,16 @@ class Video(models.Model):
         help_text='Please input "lng,latt" format'
     )
 
-
     def __str__(self):
         return str(self._id)
 
+
+class Person(models.Model):
+    _id = models.AutoField(primary_key=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    bbox_img_path = models.TextField()
+    feature_path = models.TextField()
+    time = models.DateTimeField()
 
 class TestVideo(models.Model):
     video = models.FileField(upload_to='assets/')
