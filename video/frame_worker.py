e3ac3409a3685b25ec5cd808fc355afcdea1a371
punkyoon <cryptography@konkuk.ac.kr>
2017-10-12 11:54:22 +0000

    get file list

--- video/frame_worker.py
+++ video/frame_worker.py
@@ -3,27 +3,28 @@ import av
 import numpy as np
 
 
-def extract_video_frame_array(file_path, interval=30):
-    file_name = os.path.basename(file_path)
-    folder_path = os.path.dirname(os.path.abspath(file_path))
+def extract_video_frame_array(file_list, interval=30):
+    for file_path in file_list:
+        file_name = os.path.basename(file_path)
+        folder_path = os.path.dirname(os.path.abspath(file_path))
 
-    print(file_name)
-    print(folder_path)
-    '''
-    try:
-        os.mkdir(full_path)
-    except:
-        print('Folder already exists..')
-    '''
+        print(file_name)
+        print(folder_path)
+        '''
+        try:
+            os.mkdir(full_path)
+        except:
+            print('Folder already exists..')
+        '''
 
-    video = av.open(file_path)
-    pass_count = 0
-    for frame in video.decode(video=0):
-        if pass_count % interval == 0:
-            img = frame.to_image()
-            arr = np.array(img)
+        video = av.open(file_path)
+        pass_count = 0
+        for frame in video.decode(video=0):
+            if pass_count % interval == 0:
+                img = frame.to_image()
+                arr = np.array(img)
             # using detect function...
-        pass_count += 1
+            pass_count += 1
 
 
 def extract_video_frame_image(file_path, interval=30):
@@ -47,7 +48,10 @@ def extract_video_frame_image(file_path, interval=30):
 
 '''
 if __name__ == '__main__':
-    file_path = '/home/punk/data-picker/assets/test.mp4'
-    extract_video_frame_array(file_path)
+    file_list = [
+        #'/home/punk/data-picker/assets/test.mp4',
+        '/home/punk/data-picker/assets/bmw.mp4',
+    ]
+    extract_video_frame_array(file_list)
     #extract_video_frame_image(file_path)
 '''
