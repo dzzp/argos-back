5f3463be9eee5f59eff9ad9f6bfb84c5bfab11d1
punkyoon <cryptography@konkuk.ac.kr>
2017-10-27 18:54:44 +0900

    probe_worker modified

--- video/probe_worker.py
+++ video/probe_worker.py
@@ -9,14 +9,18 @@ from video.models import Person
 
 _BATCH_SIZE = 50
 
+_BASE_DIR = '/home/dzzp/Desktop/argos-back/'
+
 _CAFFE_PATH = '/home/dzzp/Desktop/caffe/'
 _CAFFE_CMD = os.path.join(_CAFFE_PATH, 'build/tools/extract_features')
 
 _SPINDLE_PATH = os.path.join(settings.BASE_DIR, 'SpindleNet')
-_SPINDLE_MODEL = os.path.join(_SPINDLE_PATH, 'spindlenet/spindlenet_iter_50000.caffemodel')
+_SPINDLE_MODEL = os.path.join(_SPINDLE_PATH, 'spindlenet_iter_50000.caffemodel')
 
-_RPN_PATH = os.path.join(settings.BASE_DIR, 'RPN')
+#_RPN_PATH = os.path.join(settings.BASE_DIR, 'RPN')
+_RPN_PATH = os.path.join(_BASE_DIR, 'RPN')
 _RPN_INF_CMD = 'python ' + _RPN_PATH + '/inference.py'
+_RPN_PROTO_PATH = os.path.join(_RPN_PATH, 'spindlenet_test.prototxt')
 
 _CONVERT_CMD = 'python ' + _RPN_PATH + '/convert_lmdb_to_numpy.py'
 
@@ -33,11 +37,10 @@ def feature_extract(video_path):
 
     subprocess.call(_RPN_INF_CMD + ' file_list.txt ' + after_rpn, shell=True)
     subprocess.call(
-        '{caffe_cmd} {spindle_path} {spindle_model} {rpn_proto} fc7/spindle,label {feature_lmdb},{label_lmdb} {iter_len} lmdb GPU 0'.format(
+        '{caffe_cmd} {spindle_model} {rpn_proto} fc7/spindle,label {feature_lmdb},{label_lmdb} {iter_len} lmdb GPU 0'.format(
               caffe_cmd=_CAFFE_CMD,
-              spindle_path=_SPINDLE_PATH,
               spindle_model=_SPINDLE_MODEL,
-              rpn_proto=os.path.join(_RPN_PATH, '/spindlenet_text.prototxt'),
+              rpn_proto= _RPN_PROTO_PATH,
               feature_lmdb=os.path.join(_RPN_PATH, '/features_lmdb'),
               label_lmdb=os.path.join(_RPN_PATH, '/label_lmdb'),
               iter_len=iter_len
